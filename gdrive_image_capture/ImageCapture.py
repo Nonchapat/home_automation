ff7cc79e64172a1ddec217a26c3ac25cd97ef924
Tosakanth Project <tosakun.meeting@gmail.com>
2015-05-07 12:18:14 +0700

    Update ImageCapture.py

--- gdrive_image_capture/ImageCapture.py
+++ gdrive_image_capture/ImageCapture.py
@@ -1,53 +1,52 @@
-####################################
+###################################
 ###  Author : Somchai Somphadung ###
-###  Date : 2014-09-13           ###
+###  Date : 2014-09-20           ###
 ### N3A Media                    ###
-####################################
+##################################
 
 import pygame
 import pygame.camera
 import pygame.image
 import time
+import ConfigParser
+
+conf_file="gdrive.conf"
 
 class ImageCapture:
-    def __init__(self,imgsize=(320,240),pic_format="RGB",loc="/home/pi/"):
-          # pic_format could be RGB, YUV, HSV
-          pygame.init()
-          pygame.camera.init()
-          cameras = pygame.camera.list_cameras()
-          if not cameras :
-                    raise ValueError("There is not camera attached")
-                    self.camera=None
-          self.camera=pygame.camera.Camera(cameras[0],imgsize,pic_format)
-          self.is_end=False
-          self.img_loc=loc
 
+ def __init__(self,imgsize=(320,240),pic_format="RGB",loc="/home/pi/"):
+  # pic_format could be RGB, YUV, HSV
+  config  = ConfigParser.ConfigParser()
+  config.read(conf_file)
+  pygame.init()
+  pygame.camera.init()
+  cameras = pygame.camera.list_cameras()
+  if not cameras :
+    raise ValueError("There is not camera attached")
+    self.camera=None
+  
+  w=config.get('captured_image','width')
+  h=config.get('captured_image','height')
+  picsize=(int(w),int(h))
+  picformat=str(config.get('captured_image','format'))
+  self.camera=pygame.camera.Camera(cameras[0],picsize,picformat)
+  self.is_end=False
+  self.img_loc=config.get('captured_image','folder')
 
-    def create_img_name(self):
-          exp="%Y_%m_%d-%H_%M_%S"
-          return "snap_"+time.strftime(exp)+".jpeg"
+ def create_img_name(self):
+  exp="%Y_%m_%d-%H_%M_%S"
+  return "snap_"+time.strftime(exp)+".jpeg"
 
+ def capture(self):
+  if self.camera is not None :
+   self.camera.start()
+   snapshot = self.camera.get_image()
+   fname = self.img_loc+"/"+self.create_img_name()
+   pygame.image.save(snapshot,fname)
+   self.camera.stop()
+   return fname
+  else :
+   return None
 
-    def capture(self):
-          if self.camera is not None :
-          self.camera.start()
-          snapshot = self.camera.get_image()
-          pygame.image.save(snapshot,self.img_loc+"/"+self.create_img_name())
-          self.camera.stop()
-          
-    def lapse_capture(self,dur=10):
-          # dur contains duration of operation in seconds
-          if self.camera is not None :
-             self.end_time = time.time()+dur
-             self.current_time = time.time()
-             while self.current_time < self.end_time :
-                self.camera.start()
-                snapshot = self.camera.get_image()
-                pygame.image.save(snapshot,self.img_loc+"/"+self.create_img_name())
-                self.camera.stop()
-                time.sleep(self.time_interval)
-                self.current_time = time.time()
-             self.quit()
-    
-    def quit(self):
-        pygame.quit()
+ def quit(self):
+  pygame.quit()
